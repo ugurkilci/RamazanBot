d01632458828bf9c8a5cf47571de14eef09b52ac
Uğur KILCI <ugurbocugu8@gmail.com>
2017-05-24 21:03:57 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,140 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const prefix = '&';
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.username}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === prefix + 'yardım') {
+  	if (msg.author.bot) return;
+   	msg.reply('Yardım Sayfasına Git https://ugurkilci.github.io/RamazanBot/');
+  }
+
+  if (msg.content === 'acıktım') {
+   	if (Math.floor((Math.random() * 15) + 1) === 1) {
+   		msg.reply('Az sabret iftara az kaldı!');
+   	}else if (Math.floor((Math.random() * 15) + 1) === 2) {
+   		msg.reply('Sabreden deviş muradına ermiş!');
+   	}else if (Math.floor((Math.random() * 15) + 1) === 3) {
+   		msg.reply('Sabret kardeşim!');
+   	}else if (Math.floor((Math.random() * 15) + 1) === 4) {
+   		msg.reply('Film izle. Açlığını unutursun. :)');
+   	}else if (Math.floor((Math.random() * 15) + 1) === 5) {
+   		msg.reply('Dizi izle. Açlığını unutursun. :)');
+   	}else if (Math.floor((Math.random() * 15) + 1) === 6) {
+   		msg.reply('Oyun oyna. Açlığını unutursun. :)');
+   	}else if (Math.floor((Math.random() * 15) + 1) === 7) {
+   		msg.reply('Ders çalış açlığını unutursun! Açlığını unutursun. :D');
+   	}else if (Math.floor((Math.random() * 15) + 1) === 8) {
+   		msg.reply('Git bi gez gel. Açlığını unutursun. Güneşden gitmemeye çalış!');
+   	}else if (Math.floor((Math.random() * 15) + 1) === 9) {
+   		msg.reply('Geçecek bunlar, sen neler atlattın bu ne ki? :)');
+   	}else if (Math.floor((Math.random() * 15) + 1) === 10) {
+   		msg.reply('Büyüyünce geçer. :D');
+   	}else if (Math.floor((Math.random() * 15) + 1) === 11) {
+   		msg.reply('Ağla. :D');
+   	}else if (Math.floor((Math.random() * 15) + 1) === 12) {
+   		msg.reply('Lanet olsun dostum, hemen o elindeki çatalı yere bırak!');
+   	}else if (Math.floor((Math.random() * 15) + 1) === 13) {
+   		msg.reply('Lanet olsun dostum, hemen o elindeki kaşığı yere bırak!');
+   	}else if (Math.floor((Math.random() * 15) + 1) === 14) {
+   		msg.reply('Lanet olsun dostum, hemen o elindeki bıçağı yere bırak!');
+   	}else if (Math.floor((Math.random() * 15) + 1) === 15) {
+   		msg.reply('Başka bir şey düşünmeye çalış!');
+   	}
+  }
+
+  if (msg.content === 'susadım') {
+   	if (Math.floor((Math.random() * 15) + 1) === 1) {
+   		msg.reply('Az sabret iftara az kaldı!');
+   	}else if (Math.floor((Math.random() * 15) + 1) === 2) {
+   		msg.reply('Sabreden deviş muradına ermiş!');
+   	}else if (Math.floor((Math.random() * 15) + 1) === 3) {
+   		msg.reply('Sabret kardeşim!');
+   	}else if (Math.floor((Math.random() * 15) + 1) === 4) {
+   		msg.reply('Film izle. Susuzluğunu unutursun. :)');
+   	}else if (Math.floor((Math.random() * 15) + 1) === 5) {
+   		msg.reply('Dizi izle. Susuzluğunu unutursun. :)');
+   	}else if (Math.floor((Math.random() * 15) + 1) === 6) {
+   		msg.reply('Oyun oyna. Susuzluğunu unutursun. :)');
+   	}else if (Math.floor((Math.random() * 15) + 1) === 7) {
+   		msg.reply('Ders çalış açlığını unutursun! Susuzluğunu unutursun. :D');
+   	}else if (Math.floor((Math.random() * 15) + 1) === 8) {
+   		msg.reply('Git bi gez gel. Susuzluğunu unutursun.');
+   	}else if (Math.floor((Math.random() * 15) + 1) === 9) {
+   		msg.reply('Geçecek bunlar, sen neler atlattın bu ne ki? :)');
+   	}else if (Math.floor((Math.random() * 15) + 1) === 10) {
+   		msg.reply('Büyüyünce geçer. :D');
+   	}else if (Math.floor((Math.random() * 15) + 1) === 11) {
+   		msg.reply('Ağla. :D');
+   	}else if (Math.floor((Math.random() * 15) + 1) === 12) {
+   		msg.reply('Lanet olsun dostum, hemen o elindeki şişeyi yere bırak!');
+   	}else if (Math.floor((Math.random() * 15) + 1) === 13) {
+   		msg.reply('Lanet olsun dostum, hemen o elindeki içeceği yere bırak!');
+   	}else if (Math.floor((Math.random() * 15) + 1) === 14) {
+   		msg.reply('Lanet olsun dostum, hemen o elindeki bardağı yere bırak!');
+   	}else if (Math.floor((Math.random() * 15) + 1) === 15) {
+   		msg.reply('Başka bir şey düşünmeye çalış!');
+   	}
+  }
+
+  if (msg.content === 'of') {
+   	msg.reply('Oflama geçer bugünler!');
+  }
+
+  if (msg.content === 'ah') {
+   	msg.reply('Ah deme oh de!');
+  }
+
+  if (msg.content === 'oh') {
+   	msg.reply('Oh deme püf de!');
+  }
+
+  if (msg.content === 'püf') {
+   	msg.reply('Git, Barış MANÇO nun -Lambaya Püf De- dinle!');
+  }
+
+  if (msg.content === 'iftara ne kadar var') {
+   	msg.reply('Şimdi öğren -> https://iftaranekadarkaldi.com/');
+  }
+
+  if (msg.content === 'iftara kaç saat var') {
+   	msg.reply('Şimdi öğren -> https://iftaranekadarkaldi.com/');
+  }
+
+  if (msg.content === 'iftara kaç dakika var') {
+   	msg.reply('Şimdi öğren -> https://iftaranekadarkaldi.com/');
+  }
+
+  if (msg.content === 'iftara kaç gün var') {
+   	msg.reply('Yok devenin bale pabucu!?');
+  }
+
+  if (msg.content === 'iftar ne zaman') {
+  	if (Math.floor((Math.random() * 4) + 1) === 1) {
+   		msg.reply('Zamanı geldiği zaman!');
+   	}else if (Math.floor((Math.random() * 4) + 1) === 2) {
+   		msg.reply('İmam uyumuş olmasın?');
+   	}else if (Math.floor((Math.random() * 4) + 1) === 3) {
+   		msg.reply('İmam bayıldı(!)');
+   	}else if (Math.floor((Math.random() * 4) + 1) === 4) {
+   		msg.reply('Biraz Google la! ;)');
+   	}
+  }
+
+  if (msg.content === 'iftara ne kadar kaldı') {
+  	if (Math.floor((Math.random() * 4) + 1) === 1) {
+   		msg.reply('Çok değil! www.com');
+   	}else if (Math.floor((Math.random() * 4) + 1) === 2) {
+   		msg.reply('Görende 3 gün aç kaldı sanacak! www.com');
+   	}else if (Math.floor((Math.random() * 4) + 1) === 3) {
+   		msg.reply('Görende 3 gün susuz kaldı sanacak! www.com');
+   	}else if (Math.floor((Math.random() * 4) + 1) === 4) {
+   		msg.reply('Biraz Google la! ;)');
+   	}
+  }
+});
+
+client.login('TOKEN');
